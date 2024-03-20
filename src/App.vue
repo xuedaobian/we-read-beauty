c9541856bc7d493f0e54e1b8f9800c8ad92cd35c
zhangmh31513 <zhangmh31513@hundsun.com>
2024-03-20 16:48:22 +0800

    add sse demo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,45 @@
 <template>
-  <HelloWorld />
+  <div>
+    <h1>Beauty</h1>
+    <div v-html="content" class="cur-active"></div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './assets/md.md'
+<script setup>
+import { ref, onMounted } from 'vue'
+import MarkdownIt from 'markdown-it';
+import { fetchEventSource } from '@microsoft/fetch-event-source';
 
-export default {
-  components: {
-    HelloWorld,
-  },
+const md = new MarkdownIt();
+const content = ref('')
+
+onMounted(() => {
+  eventSourceHandle('http://localhost:8000/send-file');
+})
+const eventSourceHandle = async (url) => {
+  const controller = new AbortController()
+  const signal = controller.signal
+
+  fetchEventSource(`${url}`, {
+    method: 'POST',
+    signal: signal,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(''),
+    onmessage(msg) {
+      if (msg.event === '') {
+        content.value += md.render(JSON.parse(msg.data));
+      }
+      else if (msg.event === 'close') {
+        controller.abort();
+      }
+    },
+    onerror(err) {
+      throw err;    //必须throw才能停止 
+    }
+  });
 }
+
+
 </script>
